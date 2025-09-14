2efec4515d75b0767495ea69d4cd24623aff2cf5
ollehasselberg <olle-hasselberg@hotmail.com>
2025-09-14 15:04:03 +0200

    Added files from local
    These files are for the inheritence method

---
+++ Arv.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP
+{
+            //HÄR SKAPAR JAG MIN BASKLASS SOM HETER "VEHICLE", SAMT GER DEN ATTRIBUT SOM "BRAND" OCH "YEAR".
+    public class Vehicle
+    {
+        public string Brand;
+        public int Year;
+
+
+            //NU LÄGGER JAG TILL EN METOD FÖR BASKLASSEN VEHICLE
+
+        public void Move()
+        {
+            Console.WriteLine("This vehicle is moving.");
+        }
+
+        public void Stop()
+        {
+            Console.WriteLine("This vehicle is slowing down.");
+        }
+
+    }
+
+        //HÄR SKAPAR JAG MIN SUBKLASS: "CARS" SOM KOMMER ÄRVA ATTRIBUT OCH METODER FRÅN "VEHICLE".
+
+    public class Car : Vehicle
+    {
+        public int NumberOfDoors;
+
+        public new void Move()
+        {
+            Console.WriteLine("This car is moving");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿namespace OOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Vehicle vehicle1 = new Vehicle();
+            vehicle1.Move();
+
+                //Jag skapar en ny bil och kallar på metoderna från mina klasser.
+            
+            Car car1 = new Car();
+            car1.Move();
+
+            Car car2 = new Car();
+            car2.Stop();
+
+        }
+    }
+}
+
+//Car har ärvt från Vehicles metoder.
